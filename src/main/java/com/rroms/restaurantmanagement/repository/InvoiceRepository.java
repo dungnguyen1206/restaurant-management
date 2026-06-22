caa68a056581782bdc382a7bc2148e59b10ae632
duong299 <vuanghien598@gmail.com>
2026-06-22 11:48:45 +0700

    alo alo

--- src/main/java/com/rroms/restaurantmanagement/repository/InvoiceRepository.java
+++ src/main/java/com/rroms/restaurantmanagement/repository/InvoiceRepository.java
@@ -6,12 +6,16 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
     @Query("SELECT sum(i.finalAmount) from Invoice i where month (i.paidAt)=:month and year(i.paidAt)=:year")
     BigDecimal findByInvoiceNumber(String invoiceNumber, int month,int year);
 
+    List<Invoice> findByStatus(String status);
+
+    List<Invoice> findByCustomerId(Long customerId);
 
 
 
